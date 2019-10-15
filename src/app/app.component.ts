aa52339cac7b1cacd164659c541fce728c6c41c9
Eloy Perez <eloy.perez@igmweb.com>
2019-10-15 09:06:33 +0200

    Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
   constructor(
     private _modalsService: ModalsService,
     private _router: Router){
-    this.title = 'pinol';
+    this.title = 'Angular Tester';
   }
 
   ngOnInit() {
